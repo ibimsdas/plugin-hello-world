65e73107a91f5da53c4ceecccb5a438754d2e47f
Mario Kehl <mkehl@mediengarage.de>
2017-07-26 09:56:59 +0200

    dummy

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -2,7 +2,8 @@
 namespace HelloWorld\Controllers;
 
 use Plenty\Plugin\Controller;
-use Plenty\Plugin\Templates\Twig;
+use Plenty\Plugin\Http\Request;
+use Plenty\Plugin\Http\Response;
 
 /**
  * Class ContentController
@@ -10,12 +11,54 @@ use Plenty\Plugin\Templates\Twig;
  */
 class ContentController extends Controller
 {
+
 	/**
-	 * @param Twig $twig
+	 * @var Request
+	 */
+	private $request;
+
+	/**
+	 * @var Response
+	 */
+	private $response;
+
+	/**
+	 * ContentController constructor.
+	 *
+	 * @param Request $request
+	 * @param Response $response
+	 */
+	public function __construct(Request $request, Response $response)
+	{
+		$this->request = $request;
+		$this->response = $response;
+	}
+
+	/**
 	 * @return string
 	 */
-	public function sayHello(Twig $twig):string
+	public function outputProducts(): string
 	{
-		return $twig->render('HelloWorld::content.hello');
+		$apiKey = $this->request->get('apiKey', '');
+		if (!strlen($apiKey) && $apiKey !== 'dummy') {
+			$this->response->make('missing/wrong api key', Response::HTTP_BAD_REQUEST);
+		}
+
+		$products = [
+			['item_number', 'name', 'lang', 'price', 'recommendation'],
+			['000-000-001', 'Produkt 1', 'de_DE', 1000, ''],
+			['000-000-002', 'Produkt 2', 'de_DE', 501, ''],
+			['000-000-003', 'Produkt 3', 'de_DE', 10, ''],
+			['000-000-004', 'Produkt 4', 'de_DE', 999, ''],
+			['000-000-005', 'Starprodukt', 'de_DE', 10, '000-000-001,000-000-003,000-000-004'],
+			['000-000-005', 'Probe 1', 'de_DE', null, '']
+		];
+
+		$callback = function($value) {
+			return implode('\t', $value);
+		};
+		$content = implode('\n', array_map($callback, $products));
+
+		return $this->response->make($content);
 	}
 }
--- src/Providers/HelloWorldRouteServiceProvider.php
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -15,7 +15,7 @@ class HelloWorldRouteServiceProvider extends RouteServiceProvider
 	 */
 	public function map(Router $router)
 	{
-		$router->get('hello', 'HelloWorld\Controllers\ContentController@sayHello');
+		$router->get('export/products', 'HelloWorld\Controllers\ContentController@outputProducts');
 	}
 
 }
